ffc55ef752bfebe8fa56b192bebd86b10e74c4eb
Elsie <elsieoscar06@gmail.com>
2025-04-15 15:35:32 +0300

    code-along

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+function About() {
+  return (
+    <>
+      <header>
+        {/* save space for navbar */}
+      </header>
+      <main>
+        <h1> This is my about component!</h1>
+      </main>
+    </>
+  );
+}
+
+export default About;
